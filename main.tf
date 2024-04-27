37bc9616b78249b43a94ed03ebedc12b9dbad910
Kirtiranjan Sahoo <49517838+KirtiLipu@users.noreply.github.com>
2024-04-27 07:56:48 +0000

    added terraform cloud

--- main.tf
+++ main.tf
@@ -4,15 +4,15 @@ provider "aws" {
 }
 
 variable "ami" {
-    type = string
-    default = "ami-09298640a92b2d12c"
-  
+  type    = string
+  default = "ami-09298640a92b2d12c"
+
 }
 
 variable "instance_type" {
-    type = string
-    default = "t2.micro"
-  
+  type    = string
+  default = "t2.micro"
+
 }
 
 resource "aws_instance" "instance1" {
@@ -25,12 +25,12 @@ resource "aws_instance" "instance1" {
   }
 }
 
- 
+
 output "instance_id" {
-    value = aws_instance.instance1.id  
+  value = aws_instance.instance1.id
 }
 
 output "public_ip" {
   description = "Public IP address of the EC2 instance"
-  value = aws_instance.instance1.public_ip
+  value       = aws_instance.instance1.public_ip
 }

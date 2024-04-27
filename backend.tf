3c6741e1041df1ce150504fdbddcdebd7326b402
Kirtiranjan Sahoo <49517838+KirtiLipu@users.noreply.github.com>
2024-04-27 07:15:24 +0000

    first commit on codespace

--- backend.tf
+++ backend.tf
@@ -3,6 +3,5 @@ terraform {
     bucket         = "terraform-backend-007"
     key            = "githubactions/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-lock"
   }
 }

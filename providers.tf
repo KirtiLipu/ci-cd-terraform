1bd54da9af2b96fa18b7e67890906f62fa50d794
Kirtiranjan Sahoo <49517838+KirtiLipu@users.noreply.github.com>
2024-04-27 07:47:43 +0000

    added terraform cloud

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.44.0"
+    }
+  }
+}
+
+
+terraform {
+  cloud {
+    organization = "kirtilipu"
+    workspaces {
+      name = "ci-cd-terraform"
+    }
+  }
+}
